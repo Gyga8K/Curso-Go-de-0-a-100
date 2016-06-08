6a995762b955f456d3eaa3da0fb6233f7aa97c86
Alejandro Arnaud <gygatk@gmail.com>
2016-06-07 23:54:58 -0400

    Actualizado archivo fuente video 28

--- 28_Funciones_p4/main.go
+++ 28_Funciones_p4/main.go
@@ -14,11 +14,29 @@ func sumar(numeros ...int) int {
 	return resultado
 }
 
+func print(cadena string, cadenas ...string) {
+	for _, c := range cadenas {
+		cadena += " "
+		cadena += c
+	}
+	fmt.Println(cadena)
+}
+
 func main() {
 
 	fmt.Println(sumar(1, 2, 3, 4))
 	fmt.Println(sumar(10, 65, 48, 75))
 	fmt.Println(sumar(22, 25))
 	fmt.Println(sumar(25))
+	fmt.Println(sumar())
+
+	numeros := []int{
+		25,
+		56,
+		32,
+	}
+	fmt.Println(sumar(numeros...))
+
+	print("Hola", "Mundo", "con", "Go")
 
 }
